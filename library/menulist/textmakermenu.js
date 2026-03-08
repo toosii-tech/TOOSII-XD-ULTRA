6cad2308de171774ba7283bbacf2435359ba9301
toosiike <toosii042@gmail.com>
2026-03-08 22:29:58 +0300

    Update textmakermenu.js

--- library/menulist/textmakermenu.js
+++ library/menulist/textmakermenu.js
@@ -18,6 +18,33 @@ const textmakerMenu = `
 ┃➤ .blackpink [ᴛᴇxᴛ]
 ┃➤ .glitch [ᴛᴇxᴛ]
 ┃➤ .fire [ᴛᴇxᴛ]
+┗❒
+
+┏❒ *ꜰᴏɴᴛ ᴄᴏɴᴠᴇʀᴛᴇʀ* ❒
+┃ _Unicode fonts — visible to all_
+┃➤ .fonts — show all options
+┃➤ .allfonts [ᴛᴇxᴛ] — preview all
+┃➤ .bold [ᴛᴇxᴛ]
+┃➤ .italic [ᴛᴇxᴛ]
+┃➤ .bolditalic [ᴛᴇxᴛ]
+┃➤ .mono [ᴛᴇxᴛ]
+┃➤ .serif [ᴛᴇxᴛ]
+┃➤ .serifbold [ᴛᴇxᴛ]
+┃➤ .serifitalic [ᴛᴇxᴛ]
+┃➤ .scriptfont [ᴛᴇxᴛ]
+┃➤ .scriptbold [ᴛᴇxᴛ]
+┃➤ .fraktur [ᴛᴇxᴛ]
+┃➤ .frakturbold [ᴛᴇxᴛ]
+┃➤ .doublestruck [ᴛᴇxᴛ]
+┃➤ .smallcaps [ᴛᴇxᴛ]
+┃➤ .bubble [ᴛᴇxᴛ]
+┃➤ .bubblebold [ᴛᴇxᴛ]
+┃➤ .square [ᴛᴇxᴛ]
+┃➤ .squarebold [ᴛᴇxᴛ]
+┃➤ .wide [ᴛᴇxᴛ]
+┃➤ .upsidedown [ᴛᴇxᴛ]
+┃➤ .strikethrough [ᴛᴇxᴛ]
+┃➤ .underline [ᴛᴇxᴛ]
 ┗❒`
 
 module.exports = textmakerMenu
